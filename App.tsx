84a8b3250603de955a807904c9ce3dba052f1e42
Aswin Karki <aswin.karki44@gmail.com>
2023-08-20 10:18:31 +0545

    added images and flatlist to display it

--- App.tsx
+++ App.tsx
@@ -1,11 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { memo } from "react";
+import {
+  Animated,
+  Dimensions,
+  Image,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+const { width, height } = Dimensions.get("screen");
+const ITEM_WIDTH = width * 0.76;
+const ITEM_HEIGHT = ITEM_WIDTH * 1.47;
+
+const images = [
+  "https://images.unsplash.com/photo-1551316679-9c6ae9dec224?w=800&q=80",
+  "https://images.unsplash.com/photo-1562569633-622303bafef5?w=800&q=80",
+  "https://images.unsplash.com/photo-1503656142023-618e7d1f435a?w=800&q=80",
+  "https://images.unsplash.com/photo-1555096462-c1c5eb4e4d64?w=800&q=80",
+  "https://images.unsplash.com/photo-1517957754642-2870518e16f8?w=800&q=80",
+  "https://images.unsplash.com/photo-1546484959-f9a381d1330d?w=800&q=80",
+  "https://images.unsplash.com/photo-1548761208-b7896a6ff225?w=800&q=80",
+  "https://images.unsplash.com/photo-1511208687438-2c5a5abb810c?w=800&q=80",
+  "https://images.unsplash.com/photo-1548614606-52b4451f994b?w=800&q=80",
+  "https://images.unsplash.com/photo-1548600916-dc8492f8e845?w=800&q=80",
+];
+
+const RenderItem = memo(({ item, index }: { item: string; index: number }) => {
+  return (
+    <View style={{ width, justifyContent: "center", alignItems: "center" }}>
+      <Image
+        source={{ uri: item }}
+        style={{
+          width: ITEM_WIDTH,
+          height: ITEM_HEIGHT,
+          resizeMode: "cover",
+        }}
+      />
+    </View>
+  );
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <FlatList
+        data={images}
+        keyExtractor={(_, index) => index.toString()}
+        scrollEventThrottle={16}
+        horizontal
+        renderItem={({ item, index }) => (
+          <RenderItem item={item} index={index} />
+        )}
+      />
     </View>
   );
 }
@@ -13,8 +62,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
